fe1bb80a44725150a86e18d13dbc0e4f8879f8d4
Leon <leoncyao@gmail.com>
2018-06-02 15:43:08 -0700

    fixed rotation to go towards mouse

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -16,21 +16,104 @@ public class Car : MonoBehaviour {
 	void Update () {
         if (currentAngle != lastAngle)
         {
-            float difference = Mathf.Abs(currentAngle - lastAngle);
-            if (2 * Mathf.PI - difference < difference)
+
+            /*
+ * 
+ * if (currentAngle < lastAngle){
+ * 
+ * difference1 =  Mathf.Abs(currentAngle - lastAngle);
+ * measures counterclockwise
+ * 
+ * difference2 =  2 * Mathf.PI - difference1;
+ * measures clockwise
+ * 
+ * 
+ * if ( difference1 < difference2){
+ *     go ccw
+ *     currentAngle += difference1 / 100;
+ * } else if (difference1 > difference2){
+       go cw
+       currentAngle -= difference2 / 100;
+ * }
+
+ * } else if (currentAngle > lastAngle) {
+ * 
+ * difference1 =  Mathf.Abs(currentAngle - lastAngle);
+ * measures clockwise
+ * 
+ * difference2 =  2 * Mathf.PI - difference1;
+ * measures counterclockwise
+ * 
+   if ( difference1 < difference2){
+ *     go cw
+ *     currentAngle -= difference1 / 100;
+ * } else if (difference1 > difference2){
+       go ccw
+       currentAngle += difference2 / 100;
+ * }
+ * 
+ * }
+ * 
+ * 
+ * 
+
+ * */
+
+
+            float difference1 = 0, difference2 = 0;
+
+            if (currentAngle < lastAngle)
             {
-                //print("going Counterclockwise");
-                difference = 2 * Mathf.PI - difference;
-                //print("difference is " + Mathf.Rad2Deg * difference);
-                float direction = currentAngle - lastAngle < 0 ? -1 : 1;
-                difference *= direction;
+
+                difference1 = Mathf.Abs(currentAngle - lastAngle);
+                //measures counterclockwise
+
+                difference2 = 2 * Mathf.PI - difference1;
+                //measures clockwise
+
+                if (difference1 < difference2)
+                {
+                    //go ccw
+                    currentAngle += difference1 / 100;
+                }
+                else if (difference1 > difference2)
+                {
+                    //go cw
+                    currentAngle -= difference2 / 100;
+                }
+
             }
-            else
+            else if (currentAngle > lastAngle)
             {
-                //print("Clockwise");
+
+                difference1 = Mathf.Abs(currentAngle - lastAngle);
+                //measures clockwise
+
+                difference2 = 2 * Mathf.PI - difference1;
+                //measures counterclockwise
+
+                if (difference1 < difference2)
+                {
+                    //go cw
+                    currentAngle -= difference1 / 100;
+                }
+                else if (difference1 > difference2)
+                {
+                    //go ccw
+                    currentAngle += difference2 / 100;
+                }
+
             }
-            currentAngle += difference / 50;
+
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Rad2Deg * currentAngle));
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                print("difference1 is " + Mathf.Rad2Deg * difference1 + " difference2 " + Mathf.Rad2Deg * difference2);
+                print("currentangle is " + Mathf.Rad2Deg * currentAngle + " lastangle is " + Mathf.Rad2Deg * lastAngle);
+            }
+
+
         }
 
         Vector3 movement = new Vector3(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle), 0);
@@ -38,7 +121,6 @@ public class Car : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0))
         {
-            
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
             lastTarget = mousePosition;
 
@@ -65,26 +147,25 @@ public class Car : MonoBehaviour {
                     angle =  2 * Mathf.PI - angle; 
                 }
                 lastAngle = angle;
-                //lastAngle += 2 * Mathf.PI;
-
-
             }
 
-            print("last Angle " + Mathf.Rad2Deg * lastAngle + "current angle " + Mathf.Rad2Deg * currentAngle);
+            //print("last Angle " + Mathf.Rad2Deg * lastAngle + "current angle " + Mathf.Rad2Deg * currentAngle);
 
         }
 
-        drawAngleArrow(currentAngle);
-        drawAngleArrow(lastAngle);
+        drawAngleArrow(currentAngle, Color.red);
+        drawAngleArrow(lastAngle, Color.green);
+
+
 
     }
-   
 
-    public void drawAngleArrow(float angle)
+
+    public void drawAngleArrow(float angle, Color color)
     {
         Vector2 start = Main.instance.getCenter();
         Vector2 unit = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        Debug.DrawLine(start, start + unit);
+        Debug.DrawLine(start, start + unit, color);
     }
 
 
